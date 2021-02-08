5cff71407a8279da67c247e6cf22b3d5ff34de2d
M. Husni Thamrin <husni.sosnet@gmail.com>
2021-02-08 20:28:43 +0700

    use scss

--- src/app/secure/secure.component.ts
+++ src/app/secure/secure.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-secure',
   templateUrl: './secure.component.html',
-  styleUrls: ['./secure.component.sass']
+  styleUrls: ['./secure.component.scss']
 })
 export class SecureComponent implements OnInit {
 
