f2e6bbed268f0fe7b460465daf5ecb67ec1c53a6
M. Husni Thamrin <husni.sosnet@gmail.com>
2021-02-28 21:19:11 +0700

    creating public module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PublicComponent } from './public/public.component';
 import { SecureComponent } from './secure/secure.component';
-import { HomeComponent } from './public/home/home.component';
-import { LoginComponent } from './public/login/login.component';
+import { PublicModule } from './public/public.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PublicComponent,
     SecureComponent,
-    HomeComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PublicModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
