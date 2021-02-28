1f15eb65341c5a9759cf79ab0fe212ec21f87bca
M. Husni Thamrin <husni.sosnet@gmail.com>
2021-02-28 21:15:26 +0700

    creating components for public pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicComponent } from './public/public.component';
+import { SecureComponent } from './secure/secure.component';
+import { HomeComponent } from './public/home/home.component';
+import { LoginComponent } from './public/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicComponent,
+    SecureComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
