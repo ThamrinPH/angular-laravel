e3753f9dd3c73c35ca9261c38f21c34e544e6794
M. Husni Thamrin <husni.sosnet@gmail.com>
2021-03-02 10:30:08 +0700

    WIP Login component

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +15,9 @@ export class LoginComponent implements OnInit {
     password: new FormControl(''),
   });
 
-  constructor( private fb: FormBuilder, private http: HttpClient) { }
+  constructor( private fb: FormBuilder, 
+    private http: HttpClient,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.form = this.fb.group({
@@ -28,7 +31,7 @@ export class LoginComponent implements OnInit {
 
     const data = {
       username: formData.email,
-      password: formData.password,
+      password: 'password',
       grant_type: 'password',
       client_id: 2,
       client_secret: 'YBWMsPjUI8g6suVdrj8EkLBwKUpxCGYPn8dKs8h7',
@@ -36,8 +39,9 @@ export class LoginComponent implements OnInit {
     }
 
     this.http.post('http://larasixcore.test/oauth/token', data).subscribe(
-      result => {
-        console.log(result);
+      (result: any) => {
+        localStorage.setItem('token', result.access_token);
+        this.router.navigate(['/secure']);
       },
       error => {
         console.log(error);
--- src/app/secure/secure.component.ts
+++ src/app/secure/secure.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaderResponse, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecureComponent implements OnInit {
 
-  constructor() { }
+  
+  user:any;
+  
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    })
+    this.http.get('http://larasixcore.test/user', {headers: headers}).subscribe(
+      result => {
+        this.user = result
+      },
+      error => {
+        console.log(error);
+      }
+    )
   }
 
 }
